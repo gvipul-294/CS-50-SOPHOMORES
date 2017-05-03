fb6e603f04c95173b261ff6ff86f42574ebb0f61
Vipul Gupta <gvipul294@gmail.com>
2017-05-03 18:00:01 +0000

    error, I had to start from value of end as 0

--- pset3/helpers.c
+++ pset3/helpers.c
@@ -4,18 +4,12 @@
 #include "helpers.h"
 
 
-/*
- * bool
- * search(int value, int array[], int n)
- *
- * Returns true if value is in array of n values, else false.
- */
 
 bool 
 search(int value, int array[], int n)
 {
   int start = 0;
-  int end = n ; 
+  int end = n-1 ; 
   int mid = (start + end) / 2;
 
   while (start <= end)
